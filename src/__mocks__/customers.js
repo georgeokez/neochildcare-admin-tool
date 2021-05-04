09ec342ed8b54dc1ad52e14154cbbc4e343497fa
georgeokez <georgeokez@gmail.com>
2021-05-04 23:29:37 +0100

    minor change

--- src/__mocks__/customers.js
+++ src/__mocks__/customers.js
@@ -126,19 +126,5 @@ export default [
     email: 'kwak.seong.min@devias.io',
     name: 'Kwak Seong-Min',
     phone: '313-812-8947'
-  },
-  {
-    id: uuid(),
-    address: {
-      country: 'USA',
-      state: 'Utah',
-      city: 'Salt Lake City',
-      street: '368 Lamberts Branch Road'
-    },
-    avatarUrl: '/static/images/avatars/avatar_10.png',
-    createdAt: 1522702800000,
-    email: 'merrile.burgett@devias.io',
-    name: 'Merrile Burgett',
-    phone: '801-301-7894'
   }
 ];
