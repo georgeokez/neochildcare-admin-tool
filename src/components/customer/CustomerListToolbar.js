a765e63233dc81e5d0b7f6e2dfb0ef2082e9dbc2
georgeokez <georgeokez@gmail.com>
2021-05-09 00:12:28 +0100

    minor code cleaning

--- src/components/customer/CustomerListToolbar.js
+++ src/components/customer/CustomerListToolbar.js
@@ -1,11 +1,6 @@
 import {
   Box,
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  InputAdornment,
-  SvgIcon
+  Button
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
 
@@ -30,31 +25,6 @@ const CustomerListToolbar = (props) => (
         Add customer
       </Button>
     </Box>
-    {/* <Box sx={{ mt: 3 }}>
-      <Card>
-        <CardContent>
-          <Box sx={{ maxWidth: 500 }}>
-            <TextField
-              fullWidth
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SvgIcon
-                      fontSize="small"
-                      color="action"
-                    >
-                      <SearchIcon />
-                    </SvgIcon>
-                  </InputAdornment>
-                )
-              }}
-              placeholder="Search customer"
-              variant="outlined"
-            />
-          </Box>
-        </CardContent>
-      </Card>
-    </Box> */}
   </Box>
 );
 
