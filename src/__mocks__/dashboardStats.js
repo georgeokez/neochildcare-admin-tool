7e1a33f93ea4636a608554d24ca148920936ee79
georgeokez <georgeokez@gmail.com>
2021-05-10 23:57:28 +0100

    added products

---
+++ src/__mocks__/dashboardStats.js
@@ -0,0 +1,11 @@
+
+export default [
+    {
+        noOfCustomers: 57,
+        noOfDoctors: 34,
+        noOfVirturalConsultations: 36,
+        noOfVirturalChecks: 24,
+        noOfVaccineToDoor: 15,
+        noOfProducts: 3,
+    }
+]
