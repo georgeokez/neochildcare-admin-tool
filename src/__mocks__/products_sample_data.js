d1b70c903019a20739171dcf3ed985dd3df01ba1
georgeokez <georgeokez@gmail.com>
2021-05-13 15:35:32 +0100

    added sample data for products

---
+++ src/__mocks__/products_sample_data.js
@@ -0,0 +1,24 @@
+
+const export VirtualWellnessChecks = [
+    {
+        product_id: 1,
+        name: "virtual wellness checks"
+        data: []
+    }
+]
+
+const export VirtualConsultations= [
+    {
+        product_id: 2,
+        name: "virtual consulattion"
+        data: []
+    }
+]
+
+const export VaccinceToDoor = [
+    {
+        product_id: 3,
+        name: "vaccine to your door step"
+        data: []
+    }
+]
